6d660130436ee1d74ff97c71b94a412348ee9fe1
Martin Vasko <mateee12@gmail.com>
2016-11-18 22:04:52 +0100

    Repaired formatting and requirements for pep8

--- cleaner.py
+++ cleaner.py
@@ -2,10 +2,12 @@ import re
 from lxml import etree
 
 
-# purpose of this class is simply in cleaning the input xml, then this cleaned xml is used in further steps
-# of collecting and assembling articles from headings, fulltexts and separators
+# purpose of this class is simply in cleaning the input xml, then this cleaned
+# xml is used in further steps of collecting and assembling articles
+# from headings, fulltexts and separators
 class Cleaner(object):
-    DOC_ATTRS = ['producer', 'version', 'pagesCount', 'mainLanguage', 'languages']
+    DOC_ATTRS = ['producer', 'version', 'pagesCount',
+                 'mainLanguage', 'languages']
     PAGE_ATTRS = ['resolution', 'width', 'height']
     BLOCK_ATTRS = ['blockType', 'l', 't', 'r', 'b']
     LINE_ATTRS = ['baseline', 'l', 't', 'r', 'b']
@@ -25,18 +27,18 @@ class Cleaner(object):
         for formatting in parsed_xml.xpath('//formatting'):
             formatting = cls.__get_text_from_char_params(formatting)
 
-        # replace hierarchy: block -> text -> par -> line, with new one: block -> par -> line
+        # replace hierarchy: block -> text -> par -> line,
+        # with new one: block -> par -> line
         for text in parsed_xml.xpath('//text'):
             parent = text.getparent()
             for child in text.getchildren():
                 parent.append(child)
             parent.remove(text)
 
-        # remove unnecessary element par from block
+        # remove unnecessary element region from block
         for region in parsed_xml.xpath('//region'):
             region.getparent().remove(region)
 
-        print(etree.tostring(parsed_xml))
         return parsed_xml
 
     # PRIVATE methods
@@ -53,8 +55,8 @@ class Cleaner(object):
 
         return node
 
-    # depending on node tag name, use corresponding desired attributes and push them into method for
-    # removing unwanted attributes
+    # depending on node tag name, use corresponding desired attributes
+    # and push them into method for removing unwanted attributes
     @classmethod
     def __remove_unwanted_attrs(cls, node):
         if node.tag == 'document':
@@ -86,5 +88,3 @@ class Cleaner(object):
         formatting.text = re.sub('[^\040-\176]', '', formatting.text)
 
         return formatting
-
-
