1feb926ae357840d1f679573ff3aa4255c0a3d8e
Jakub Hagara <jakub.hagara171@gmail.com>
2016-11-24 18:39:59 +0100

    pep8 check

--- parser/xml/discriminator/_heading.py
+++ parser/xml/discriminator/_heading.py
@@ -1,14 +1,16 @@
 import re
 from lxml import etree
-import unittest 
+import unittest
+
 
 # purpose of this class is to identify headings
 class _Heading(object):
-    #indentify all headings
+    # indentify all headings
     @classmethod
     def discriminate_headings(cls, parsed_xml):
         most_used = _Heading._most_used_fs(parsed_xml)
-        for formatting in parsed_xml.xpath('//*[local-name() = \'formatting\']'):
+        for formatting \
+                in parsed_xml.xpath('//*[local-name() = \'formatting\']'):
             fs = _Heading._get_fs(formatting)
             if fs > most_used:
                 par = formatting.getparent().getparent()
@@ -19,30 +21,29 @@ class _Heading(object):
                         if formatting_fs <= most_used:
                             is_heading = False
                 if is_heading:
-                    par.set("type","heading")
+                    par.set("type", "heading")
                     block = par.getparent()
-                    block.set("type","text")
+                    block.set("type", "text")
         return parsed_xml
 
-    #find tho most used font size
+    # find tho most used font size
     @classmethod
     def _most_used_fs(cls, parsed_xml):
         number_fs = [0] * 1000
-        for formatting in parsed_xml.xpath('//*[local-name() = \'formatting\']'):
+        for formatting \
+                in parsed_xml.xpath('//*[local-name() = \'formatting\']'):
             fs = _Heading._get_fs(formatting)
             number_fs[fs] += 1
 
         most_used = 0
-        for i in range(0,999):
+        for i in range(0, 999):
             if number_fs[i] > number_fs[most_used]:
                 most_used = i
         return most_used
 
-    #get font size from element formatting
+    # get font size from element formatting
     @classmethod
     def _get_fs(cls, formatting):
         fs_string = formatting.get("fs")
         fs = int(re.match("\d+", fs_string).group(0))
         return fs
-
-
