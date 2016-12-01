8cccdd5d722ed3b768e07ae85e06a7e9610d37c7
Martina Redajova <redajova.mata@gmail.com>
2016-12-01 13:17:56 +0100

    Check for headings added

--- tests/parser/xml/discriminator/test_separators.py
+++ tests/parser/xml/discriminator/test_separators.py
@@ -11,33 +11,33 @@ class TestSeparator(unittest.TestCase):
             <document>
                 <page width="3455" height="4871" resolution="400">
                     <block blockType="Text" l="180" t="98" r="566" b="164">
-                        <par l="0" t="0" r="100" b="100" name="a">
+                        <par l="0" t="0" r="100" b="100" type="text">
                         </par>
-                        <par l="101" t="0" r="200" b="100" name="c">
+                        <par l="101" t="0" r="200" b="100" type="heading">
                         </par>
-                        <par l="201" t="0" r="300" b="100" name="d">
+                        <par l="201" t="0" r="300" b="100" type="text">
                         </par>
                     </block>
                     <block blockType="Picture" l="0" t="120" r="300" b="147">
                     </block>
                     <block blockType="Text" l="0" t="150" r="300" b="200">
-                        <par l="0" t="150" r="300" b="200" name="b">
+                        <par l="0" t="150" r="300" b="200" type="text">
                         </par>
                     </block>
                 </page>
                 <page width="3455" height="4871" resolution="400">
                     <block blockType="Text" l="180" t="98" r="566" b="164">
-                        <par l="0" t="0" r="100" b="100" name="a">
+                        <par l="0" t="0" r="100" b="100" type="text">
                         </par>
-                        <par l="101" t="0" r="200" b="100" name="c">
+                        <par l="101" t="0" r="200" b="100" type="heading">
                         </par>
-                        <par l="201" t="0" r="300" b="100" name="d">
+                        <par l="201" t="0" r="300" b="100" type="text">
                         </par>
                     </block>
                     <block blockType="Picture" l="0" t="120" r="300" b="147">
                     </block>
                     <block blockType="Text" l="0" t="150" r="300" b="200">
-                        <par l="0" t="150" r="300" b="200" name="b">
+                        <par l="0" t="150" r="300" b="200" type="text">
                         </par>
                     </block>
                 </page>
@@ -48,36 +48,36 @@ class TestSeparator(unittest.TestCase):
             <document>
                 <page width="3455" height="4871" resolution="400">
                     <block blockType="Text" l="180" t="98" r="566" b="164">
-                        <par l="0" t="0" r="100" b="100" name="a">
+                        <par l="0" t="0" r="100" b="100" type="text">
                         </par>
-                        <par l="101" t="0" r="200" b="100" name="c">
+                        <par l="101" t="0" r="200" b="100" type="heading">
                         </par>
-                        <par l="201" t="0" r="300" b="100" name="d">
+                        <par l="201" t="0" r="300" b="100" type="text">
                         </par>
                     </block>
-                    <block blockType="Picture" l="0" t="120" r="300" b="147" type="Separator">
+                    <block blockType="Picture" l="0" t="120" r="300" b="147" type="separator">
                     </block>
                     <block blockType="Text" l="0" t="150" r="300" b="200">
-                        <par l="0" t="150" r="300" b="200" name="b">
+                        <par l="0" t="150" r="300" b="200" type="text">
                         </par>
                     </block>
-                    <block blockType="Picture" l="0" t="101" r="100" b="149" type="Separator"> </block><block blockType="Picture" l="101" t="101" r="200" b="149" type="Separator"> </block><block blockType="Picture" l="201" t="101" r="300" b="149" type="Separator"> </block></page>
+                    <block blockType="Picture" l="0" t="101" r="100" b="149" type="separator"> </block><block blockType="Picture" l="201" t="101" r="300" b="149" type="separator"> </block></page>
                 <page width="3455" height="4871" resolution="400">
                     <block blockType="Text" l="180" t="98" r="566" b="164">
-                        <par l="0" t="0" r="100" b="100" name="a">
+                        <par l="0" t="0" r="100" b="100" type="text">
                         </par>
-                        <par l="101" t="0" r="200" b="100" name="c">
+                        <par l="101" t="0" r="200" b="100" type="heading">
                         </par>
-                        <par l="201" t="0" r="300" b="100" name="d">
+                        <par l="201" t="0" r="300" b="100" type="text">
                         </par>
                     </block>
-                    <block blockType="Picture" l="0" t="120" r="300" b="147" type="Separator">
+                    <block blockType="Picture" l="0" t="120" r="300" b="147" type="separator">
                     </block>
                     <block blockType="Text" l="0" t="150" r="300" b="200">
-                        <par l="0" t="150" r="300" b="200" name="b">
+                        <par l="0" t="150" r="300" b="200" type="text">
                         </par>
                     </block>
-                    <block blockType="Picture" l="0" t="101" r="100" b="149" type="Separator"> </block><block blockType="Picture" l="101" t="101" r="200" b="149" type="Separator"> </block><block blockType="Picture" l="201" t="101" r="300" b="149" type="Separator"> </block></page>
+                    <block blockType="Picture" l="0" t="101" r="100" b="149" type="separator"> </block><block blockType="Picture" l="201" t="101" r="300" b="149" type="separator"> </block></page>
             </document>
                         """
 
@@ -85,6 +85,11 @@ class TestSeparator(unittest.TestCase):
         desired_xml = etree.fromstring(desired_xml)
 
         print (etree.tostring(actual_xml).decode('utf-8'))
+        self.assertEqual(re.sub('[^\040-\176]| ', '', etree.tostring(desired_xml).decode('utf-8')),
+                         re.sub('[^\040-\176]| ', '', etree.tostring(actual_xml).decode('utf-8')))
+
+
+
         self.assertEqual(re.sub('[^\040-\176]| ', '', etree.tostring(desired_xml).decode('utf-8')),
                          re.sub('[^\040-\176]| ', '', etree.tostring(actual_xml).decode('utf-8')))
 
