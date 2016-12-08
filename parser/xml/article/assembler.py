564e15dcfdab95f4a01875aad1f5bd426227e207
Martin Vasko <mateee12@gmail.com>
2016-12-08 21:00:41 +0100

    added 2 new helper methods for 	chaining two groups, also tests 	find last middle element, also tests

--- parser/xml/article/assembler.py
+++ parser/xml/article/assembler.py
@@ -7,18 +7,41 @@ class Assembler(object):
     # parsed_xml = None
     # ERROR = 3
 
-    def __init__(self, parsed_xml, previous_page, current_page, ERROR):
-        self.previous_page = previous_page
-        self.current_page = current_page
+    def __init__(self, parsed_xml, **args):
+        default = {'previous_page': None, 'current_page': None, 'ERROR': 3,
+                   'current_page_num': None, 'chains': {}, 'chains_mapper': {}, 'last_chain_num': 0}
+        args = {**default, **args}
+
+        self.previous_page = args['previous_page']
+        self.current_page = args['current_page']
+        self.current_page_num = args['current_page_num']
         self.parsed_xml = parsed_xml
-        self.ERROR = ERROR
+        self.ERROR = args['ERROR']
+        self.chains = args['chains']
+        self.chains_mapper = args['chains_mapper']
+        self.last_chain_num = args['last_chain_num']
+
+    # def __init__(self, parsed_xml, previous_page=None, current_page=None, ERROR=3,
+    #              current_page_num=None, chains={}, chains_mapper={}, last_chain_num=0):
+    #     self.previous_page = previous_page
+    #     self.current_page = current_page
+    #     self.current_page_num = current_page_num
+    #     self.parsed_xml = parsed_xml
+    #     self.ERROR = ERROR
+    #     self.chains = chains
+    #     self.chains_mapper = chains_mapper
+    #     self.last_chain_num = last_chain_num
 
     def assembly_articles(self, parsed_xml):
         self.parsed_xml = parsed_xml
 
+        i = 0
         # first cycle of all groups in pages
         for page in parsed_xml.xpath("/document/page"):
             self.current_page = page
+            i += 1
+            self.current_page_num = i
+            self.chains[i] = {}
 
             for group in page.xpath("group"):
 
@@ -34,14 +57,18 @@ class Assembler(object):
             self.previous_page = page
 
         self.previous_page = None
+        self.current_page_num = None
 
+        i = 0
         # second cycle of all ALONE in the dark groups
         for page in parsed_xml.xpath("/document/page"):
             self.current_page = page
+            i += 1
+            self.current_page_num = i
 
-            for group in page.xpath("group[@chained]"):
+            for group in page.xpath("group[not(@chained)]"):
                 group.attrib['column_position'] = \
-                    self._find_column_position(group)
+                    self.__find_column_position(group)
 
                 if group.attrib['column_position'] == 'left':
                     chained = self.__chainable_left_alone(group)
@@ -108,9 +135,32 @@ class Assembler(object):
     # PRIVATE HELPER METHODS
 
     # chain together two groups, creat associations between them
-    @classmethod
-    def __chain_groups(cls, group1, group2):
+    def __chain_groups(self, group1, group2):
         # chain groups
+        id1 = id(group1)
+        id2 = id(group2)
+        chain_num_same1 = self.chains_mapper.get(id1)
+        chain_num_same2 = self.chains_mapper.get(id2)
+
+        # chain group already exist
+        if (chain_num_same1 is not None) or (chain_num_same2 is not None):
+            if (chain_num_same1 is not None):
+                chain_num = chain_num_same1
+                self.chains[self.current_page_num][chain_num].append(group2)
+                self.chains_mapper[id2] = chain_num
+            else:
+                chain_num = chain_num_same2
+                self.chains[self.current_page_num][chain_num].append(group1)
+                self.chains_mapper[id1] = chain_num
+        # chain group not exist
+        else:
+            # plus 1 - we're gonna create new chain group
+            self.last_chain_num += 1
+            # add groups to newly created group
+            self.chains[self.current_page_num][self.last_chain_num] = [group1, group2]
+            # add knowledge of appending groups to chains mapper
+            self.chains_mapper[id1] = self.last_chain_num
+            self.chains_mapper[id2] = self.last_chain_num
 
         # mark groups as already chained, as not ALONE, attrib chained=true
         group1.attrib['chained'] = 'true'
@@ -118,14 +168,14 @@ class Assembler(object):
 
     # get column position
     def __find_column_position(self, group):
-        left = self.__find_nearest_left(group)
-        right = self.__find_nearest_right(group)
+        exist_left = self.__exist_any_on_the_left(group)
+        exist_right = self.__exist_any_on_the_right(group)
 
-        if (left is not None) and (right is not None):
+        if exist_left and exist_right:
             return 'middle'
-        elif (left is None) and (right is not None):
+        elif not exist_left and exist_right:
             return 'left'
-        elif (left is not None) and (right is None):
+        elif exist_left and not exist_right:
             return 'right'
         else:
             return 'left'
@@ -189,14 +239,54 @@ class Assembler(object):
 
         return self.__get_min_or_max(results, 100000, 'l', operator.lt)
 
+    def __exist_any_on_the_left(self, group):
+        """find all groups on the left from current group
+
+        :param group:lxml.etree._Element
+        :return: group:lxml.etree._Element or None
+        """
+
+        l = int(group.attrib['l']) + self.ERROR
+        results = self.current_page.xpath("group[@type != 'separators' and @r <= " + str(l) + "][1]")
+        if len(results) != 0:
+            return True
+        else:
+            return False
+
+    def __exist_any_on_the_right(self, group):
+        """find all groups on the left from current group
+
+        :param group:lxml.etree._Element
+        :return: group:lxml.etree._Element or None
+        """
+
+        r = int(group.attrib['r']) - self.ERROR
+        results = self.current_page.xpath("group[@type != 'separators' and @l >= " + str(r) + "][1]")
+        if len(results) != 0:
+            return True
+        else:
+            return False
+
     # Jozef
-    @classmethod
-    def __find_last_middle_alone(cls):
-        """find last ALONE group element located in middle column
+    def __find_last_middle(self):
+        """find last group element located in middle column
 
         :param
         :return: group:lxml.etree._Element or None
         """
+
+        groups = self.current_page.xpath("group[@column_position = 'middle']")
+        most_right = self.__get_min_or_max(groups, -1, 'r', operator.gt)
+
+        if most_right is not None:
+            r = int(most_right.attrib['r'])
+            query = "group[@column_position = 'middle' and @r >= " \
+                    "" + str(r - self.ERROR) + " and @r <= " + str(r + self.ERROR) + "]"
+            groups = self.current_page.xpath(query)
+            most_bottom = self.__get_min_or_max(groups, -1, 'b', operator.gt)
+            return most_bottom
+        else:
+            return None
 
     def __find_middle_alone(self):
         """find any ALONE group element located in middle column
