cecd59225a7639b8e4840cbf90a913d8886ce2db
Jakub Hagara <jakub.hagara171@gmail.com>
2016-12-09 16:35:25 +0100

    Change tests
    Change tests for 2Dii and assembler

--- tests/parser/xml/article/test_assembler_all.py
+++ tests/parser/xml/article/test_assembler_all.py
@@ -6,17 +6,13 @@ import re
 
 class Testassemblerall(unittest.TestCase):
     def test_assembler_all_success(self):
-
-        desired_output = """"""
-
-        chained_groups = \
-            Assembler.assembly_articles(etree.parse("input_test_assembler.xml"))
-        actual_output = ""
-        for group in chained_groups:
-            for g in group:
-                actual_output = actual_output + etree.tostring(g).decode('utf-8')
-
-        self.assertEqual(re.sub('[^\040-\176]| ', '',desired_output),
-                         re.sub('[^\040-\176]| ', '',actual_output))
-
+        assembler = Assembler()
+        Assembler.assembly_articles(etree.parse("input_test_assembler.xml"))
+        chains = assembler.chains
+        for i, page in enumerate(chains):
+            print("page" + i)
+            for j, chain in enumerate(page):
+                print("chain" + j)
+                for group in chain:
+                    print(group.xpath('.//formatting').text)
 
