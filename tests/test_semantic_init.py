cd964230f6ea2d53e7afbb1bb02debe7f53f7405
Adam Rafajdus <adam.rafajdus@gmail.com>
2017-01-27 14:53:48 +0100

    Refactor to PEP8 and better readability
    Signed-off-by: Adam Rafajdus <adam.rafajdus@gmail.com>

--- tests/test_semantic_init.py
+++ tests/test_semantic_init.py
@@ -5,8 +5,9 @@ from semantic import Semantic
 
 class TestSemanticInit(unittest.TestCase):
     def test_basic_init_test(self):
-        header_conf_path = os.path.dirname(os.path.abspath(__file__)) + "/page_header_conf_1941_1.json"
-        xml_path = os.path.dirname(os.path.abspath(__file__)) + "/slovak_1941_1_strana_1.xml"
+        abs_path = os.path.dirname(os.path.abspath(__file__))
+        header_conf_path = abs_path + "/page_header_conf_1941_1.json"
+        xml_path = abs_path + "/slovak_1941_1_strana_1.xml"
         semantic = Semantic(xml=xml_path, header_config=header_conf_path)
         i = 7
 
