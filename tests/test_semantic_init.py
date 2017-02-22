2a190d2f1089840d3287ec8c080be74ec3122bcf
Adam Rafajdus <adam.rafajdus@gmail.com>
2017-02-22 20:26:21 +0100

    Add test for full size xml of semantic generation
    Faily for now, Martin gogo!
    
    Signed-off-by: Adam Rafajdus <adam.rafajdus@gmail.com>

--- tests/test_semantic_init.py
+++ tests/test_semantic_init.py
@@ -17,6 +17,12 @@ class TestSemanticInit(unittest.TestCase):
         xml_path = abs_path + "/slovak_1941_1_strana_1.xml"
         semantic = Semantic(xml=xml_path, header_config=header_conf_path)
 
+    def test_basic_bigger_test(self):
+        abs_path = os.path.dirname(os.path.abspath(__file__))
+        header_conf_path = abs_path + "/page_header_conf_1941_1.json"
+        xml_path = abs_path + "/slovak_1939_05_26_full.xml"
+        semantic = Semantic(xml=xml_path, header_config=header_conf_path)
+
     def test_preprocess_xml(self):
         abs_path = os.path.dirname(os.path.abspath(__file__))
         # just cleaned part
