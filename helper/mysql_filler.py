820e4c40052f96fbeb62a4131bb92733e497557f
Adam Rafajdus <adam.rafajdus@gmail.com>
2017-02-22 19:13:34 +0100

    Add default empty function for db
    Mysql to be precise
    
    Signed-off-by: Adam Rafajdus <adam.rafajdus@gmail.com>

---
+++ helper/mysql_filler.py
@@ -0,0 +1,112 @@
+import pymysql
+
+class Filler(object):
+
+    def fill_articles(self, issue_name):
+
+        db = pymysql.connect(host="127.0.0.1",
+                             user="root",
+                             password="rootroot",
+                             database="tp")
+
+        print('Connection estalished')
+        with db.cursor() as cursor:
+                # Create a new record
+                sql = "SELECT id FROM issues"
+                cursor.execute(sql)
+
+            # connection is not autocommit by default. So you must commit to save
+            # your changes.
+            db.commit()
+
+            create_issue = True
+
+            while True:
+                row = cursor.fetchone()
+                if row is None:
+                    break
+                else row[1] is issue_name:
+                    create_issue = False
+                    issue_id = row[0]
+                    print("%s" % row[0])
+
+        if (create_issue):
+            with db.cursor() as cursor:
+                    # Create a new record
+                    sql = "INSERT INTO `issues` (`name`) VALUES (%s)"
+                    cursor.execute(sql, (issue_name))
+
+                # connection is not autocommit by default. So you must commit to save
+                # your changes.
+                try:
+                    db.commit()
+                    issue_id = cursor.lastrowid
+                except:
+                    db.rollback()
+
+        with db.cursor() as cursor:
+                # Create a new record
+                sql = "INSERT INTO `articles` (`heading`, `ftext`, 'issue_id') VALUES (%s, %s)"
+                cursor.execute(sql, (heading, ftext, issue_id))
+
+            # connection is not autocommit by default. So you must commit to save
+            # your changes.
+            try:
+                db.commit()
+            except:
+                db.rollback()
+
+
+    def fill_mark_21(self, issue_name):
+
+        db = pymysql.connect(host="127.0.0.1",
+                             user="root",
+                             password="rootroot",
+                             database="tp")
+
+        print('Connection estalished')
+        with db.cursor() as cursor:
+                # Create a new record
+                sql = "SELECT id FROM issues"
+                cursor.execute(sql)
+
+            # connection is not autocommit by default. So you must commit to save
+            # your changes.
+            db.commit()
+
+            create_issue = True
+
+            while True:
+                row = cursor.fetchone()
+                if row is None:
+                    break
+                else row[1] is issue_name:
+                    create_issue = False
+                    issue_id = row[0]
+                    print("%s" % row[0])
+
+        if (create_issue):
+            with db.cursor() as cursor:
+                    # Create a new record
+                    sql = "INSERT INTO `issues` (`name`) VALUES (%s)"
+                    cursor.execute(sql, (issue_name))
+
+                # connection is not autocommit by default. So you must commit to save
+                # your changes.
+                try:
+                    db.commit()
+                    issue_id = cursor.lastrowid
+                except:
+                    db.rollback()
+
+        with db.cursor() as cursor:
+                # Create a new record
+                sql = "INSERT INTO 'mark_21` (`dict_key`, `value`) VALUES (%s, %s)"
+                cursor.execute(sql, (dict_key, value))
+
+            # connection is not autocommit by default. So you must commit to save
+            # your changes.
+            try:
+                db.commit()
+            except:
+                db.rollback()
---
+++ helper/mysql_init_tables.py
@@ -0,0 +1,51 @@
+import pymysql
+
+print('Preparing connection')
+db = pymysql.connect(host="127.0.0.1",
+                     user="root",
+                     password="rootroot",
+                     database="tp")
+
+print('Connection estalished')
+cursor = db.cursor()
+
+# cursor.execute("DROP TABLE IF EXISTS issues, articles, mark_21;")
+
+query_create_issues = """
+        CREATE TABLE IF NOT EXISTS issues (
+        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        name TEXT CHARACTER SET utf8
+        );
+        """
+
+query_create_articles = """
+        CREATE TABLE IF NOT EXISTS articles (
+        id INT NOT NULL AUTO_INCREMENT,
+        heading TEXT CHARACTER SET utf8,
+        ftext TEXT CHARACTER SET utf8,
+        issue_id INT,
+        PRIMARY KEY (id),
+        FOREIGN KEY (issue_id) REFERENCES issues(id)
+        );
+        """
+
+query_create_mark21 = """
+        CREATE TABLE IF NOT EXISTS mark_21 (
+        id INT NOT NULL AUTO_INCREMENT,
+        dict_key TEXT CHARACTER SET utf8,
+        value TEXT CHARACTER SET utf8,
+        issue_id INT,
+        PRIMARY KEY (id),
+        FOREIGN KEY (issue_id) REFERENCES issues(id)
+        );
+        """
+
+print('Execuring query #1')
+cursor.execute(query_create_issues)
+print('Execuring query #2')
+cursor.execute(query_create_articles)
+print('Execuring query #3')
+cursor.execute(query_create_mark21)
+
+print('Closing db')
+db.close()
