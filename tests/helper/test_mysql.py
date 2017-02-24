eef78cb5d755827b35c7134210aadae00efc73f0
Adam Rafajdus <adam.rafajdus@gmail.com>
2017-02-24 13:54:01 +0100

    Add hollow test for mysql db
    Trying TDD
    
    Signed-off-by: Adam Rafajdus <adam.rafajdus@gmail.com>

---
+++ tests/helper/test_mysql.py
@@ -0,0 +1,13 @@
+import unittest
+import os
+import pymysql
+
+class TestDB(unittest.TestCase):
+    def test_connection(self):
+        assert True
+    
+    def test_tables(self):
+        assert True
+
+    def test_filler(self):
+        assert True
