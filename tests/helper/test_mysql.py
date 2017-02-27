9418afc4e241e325a68858e273b23ecae22f1244
Adam Rafajdus <adam.rafajdus@gmail.com>
2017-02-27 02:38:18 +0100

    Rework all db tests
    Kinda used TDD, test are working on local mashine with created
    db with name 'text' as for testing purposes.
    Don't really like this implementation, but there is no other way,
    without docker or so, to test this.
    May be changed soon with travisCI.
    
    Signed-off-by: Adam Rafajdus <adam.rafajdus@gmail.com>

--- tests/helper/test_mysql.py
+++ tests/helper/test_mysql.py
@@ -1,13 +1,73 @@
 import unittest
 import os
 import pymysql
+from semantic import Semantic
+from helper import mysql_filler
+from helper import mysql_init_tables
+from helper import mysql_drop_tables
 
 class TestDB(unittest.TestCase):
-    def test_connection(self):
-        assert True
-    
+
     def test_tables(self):
-        assert True
+        db_name = 'text'
+        mysql_init_tables.init_tables(db_name)
+
+        db = pymysql.connect(host="127.0.0.1",
+                             user="root",
+                             password="rootroot",
+                             database=db_name)
+
+        print('Connection estalished')
+        with db.cursor() as cursor:
+            sql = "show tables"
+            cursor.execute(sql)
+            db.commit()
+            row = cursor.fetchall()
+        
+        self.assertEqual(row[0][0], 'articles')
+        self.assertEqual(row[1][0], 'issues')
+        self.assertEqual(row[2][0], 'marc21')
+
+
+        mysql_drop_tables.drop_tables(db_name)
+
+        db = pymysql.connect(host="127.0.0.1",
+                             user="root",
+                             password="rootroot",
+                             database=db_name)
+
+        print('Connection estalished')
+        with db.cursor() as cursor:
+            sql = "show tables"
+            cursor.execute(sql)
+            db.commit()
+            row = cursor.fetchall()
+
+        self.assertEqual(len(row), 0)
 
     def test_filler(self):
-        assert True
+        db_name = 'text'
+        abs_path = os.path.dirname(os.path.abspath(__file__))
+        header_conf_path = abs_path + "/page_header_conf_1941_1.json"
+        xml_path = abs_path + "/slovak_1941_1_strana_1.xml"
+        semantic = Semantic(xml=xml_path, header_config=header_conf_path)
+        mysql_init_tables.init_tables(db_name)
+        mysql_filler.Filler.fill_articles(semantic, 'Slovak_Test', db_name)
+        mysql_filler.Filler.fill_marc21(semantic, 'Slovak_Test', db_name)
+
+        db = pymysql.connect(host="127.0.0.1",
+                             user="root",
+                             password="rootroot",
+                             database=db_name)
+
+        print('Connection estalished')
+        with db.cursor() as cursor:
+            sql = "select * from articles"
+            cursor.execute(sql)
+            db.commit()
+            row = cursor.fetchall()
+
+        self.assertEqual(len(row), 6)
+        self.assertEqual(len(row[0]), 4)
+        self.assertEqual(len(row[2][1]), len(' Prielom slovenského vojska cez nepriate?ské opevnené línie'))
+        self.assertEqual(row[2][1], ' Prielom slovenského vojska cez nepriate?ské opevnené línie')
