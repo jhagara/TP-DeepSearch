3ed7c418aedbd4593a762f1eededf703a60014cb
Jakub Hagara <jakub.hagara171@gmail.com>
2017-03-18 00:12:43 +0100

    First implementation of keywords
    Implementation using tf-idf. Not tested yet.

--- helper/elastic_filler.py
+++ helper/elastic_filler.py
@@ -1,6 +1,8 @@
 import copy
 import json
+import math
 from elasticsearch import Elasticsearch
+from textblob import TextBlob as tB
 
 
 class Elastic(object):
@@ -114,3 +116,68 @@ class Elastic(object):
             print("Article created with id: ", some['_id'])
 
         es.indices.refresh(index="issues")
+
+    def __tf(self, word, blob):
+        return blob.words.count(word) / len(blob.words)
+
+    def __n_containing(self, word, bloblist):
+        return sum(1 for blob in bloblist if word in blob)
+
+    def __idf(self, word, bloblist):
+        return math.log(len(bloblist) / (1 + self.__n_containing(word, bloblist)))
+
+    def __tfidf(self, word, blob, bloblist):
+        return self.__tf(word, blob) * self.__idf(word, bloblist)
+
+    def key_words(self, bloblist):
+
+        key_words_art = []
+
+        for i, blob in enumerate(bloblist):
+            scores = {word: self.__tfidf(word, blob, bloblist) for word in blob.words}
+            sorted_words = sorted(scores.items(), key=lambda x: x[1], reverse=True)
+            key_words_art.append(sorted_words[:10])
+
+        return key_words_art
+
+    def key_words_from_json(self, jlist):
+
+        bloblist = []
+
+        for article in jlist:
+            all_text = ''
+            for group in article['groups']:
+                all_text += group['text']
+            bloblist.append(tB(all_text))
+
+        key_words_art = self.key_words(bloblist)
+        for i, article in jlist:
+            article['keywords'] = key_words_art[i]
+
+        return
+
+    def insert_key_words(self, id):
+
+        # establishment of connection
+        es = Elasticsearch()
+
+        result = es.search(index="issues", doc_type="article", body={"query": {"match": {"id": id}}})
+
+        article_ids = []
+        article_bodies = []
+
+        for hit in result['hits']['hits']:
+            article_bodies.append(hit['_source'])
+            article_ids.append(hit['_id'])
+
+        self.key_words_from_json(article_bodies)
+
+        for i, article in enumerate(article_bodies):
+            updated = es.index(index='issues', doc_type='article', body=article, id=article_ids[i])
+            if updated['_id'] != article_ids[i]:
+                print("hlaska")  #TODO doplnit
+                return 1
+
+        es.indices.refresh(index="issues")
+
+
