59aac2550d8d9cc505d15cdcfbcdba1d8bff8793
Jakub Hagara <jakub.hagara171@gmail.com>
2017-04-20 15:05:00 +0200

    Add third cycle for alone groups

--- parser/xml/article/assembler.py
+++ parser/xml/article/assembler.py
@@ -74,6 +74,24 @@ class Assembler(object):
 
             self.previous_page = page
 
+        self.previous_page = None
+        self.current_page_num = None
+
+        i = 0
+        # third cycle of all ALONE in the dark groups, just grouped in one article on page
+        for page in self.parsed_xml.xpath("/document/page"):
+            self.current_page = page
+            i += 1
+            self.current_page_num = i
+
+            groups = page.xpath("group[not(@chained)]")
+            alone_article = groups[0]
+
+            for group in groups[1:]:
+                self.__chain_groups(alone_article, group)
+
+            self.previous_page = page
+
         self.__order_groups_and_create_array()
 
     # PRIVATE
