be98b5d4a947892e01c33a18f028dd04c64d9945
Jakub Hagara <jakub.hagara171@gmail.com>
2017-07-13 18:50:39 +0200

    Add xml validator

---
+++ parser/xml/validator.py
@@ -0,0 +1,36 @@
+from lxml import etree
+import os
+
+
+class Validator(object):
+    INPUT_SCHEMA = "/../../schemas/input_schema_v2.xsd"
+
+    def validate(self, xml, schema):
+        if xml is None or schema is None:
+            return -1
+        return schema.validate(xml)
+
+    def validate_xml_schema(self, xml_path, schema_path):
+        # load xml file to init stage
+        xml = etree.parse(xml_path)
+
+        # load schema from file
+        schema_doc = etree.parse(schema_path)
+        schema = etree.XMLSchema(schema_doc)
+
+        return self.validate(xml, schema)
+
+    def validate_schema(self, xml, schema_path):
+        # load schema from file
+        schema_doc = etree.parse(schema_path)
+        schema = etree.XMLSchema(schema_doc)
+
+        return self.validate(xml, schema)
+
+    def validate_inputxml(self, xml):
+        # load input schema from file
+        schema_path = os.path.dirname(os.path.abspath(__file__)) + self.INPUT_SCHEMA
+        schema_doc = etree.parse(schema_path)
+        schema = etree.XMLSchema(schema_doc)
+
+        return self.validate(xml, schema)
---
+++ tests/parser/xml/test_validator.py
@@ -0,0 +1,21 @@
+import unittest
+import os
+from lxml import etree
+from parser.xml.validator import Validator
+
+
+class MyTestCase(unittest.TestCase):
+    def test_validate_inputxml(self):
+        validator = Validator()
+        valid_path = os.path.dirname(os.path.abspath(__file__)) + "/valid.xml"  # path to valid xml
+        valid_xml = etree.parse(valid_path)
+        result = validator.validate_inputxml(valid_xml)
+        self.assertEqual(True, result)
+
+        invalid_path = os.path.dirname(os.path.abspath(__file__)) + "/invalid.xml"  # path to invalid xml
+        invalid_xml = etree.parse(invalid_path)
+        result = validator.validate_inputxml(invalid_xml)
+        self.assertEqual(False, result)
+
+if __name__ == '__main__':
+    unittest.main()
