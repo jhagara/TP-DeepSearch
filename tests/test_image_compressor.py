836f1c044bc69254f66c872ef55d0095922e9a20
Adam Rafajdus <adam.rafajdus@gmail.com>
2017-07-21 00:50:45 +0200

    Add a new test for image compression
    Signed-off-by: Adam Rafajdus <adam.rafajdus@gmail.com>

---
+++ tests/test_image_compressor.py
@@ -0,0 +1,36 @@
+import unittest
+import os
+import shutil
+from helper.image_processor import ImageProcessor
+from PIL import Image
+import glob
+
+
+class TestImageExport(unittest.TestCase):
+
+    def test_create_images(self):
+        pics_dirname = os.path.dirname(os.path.abspath(__file__)) + "/19430526"
+
+        rem_dir = pics_dirname + "/STR_small"
+
+        pics_dirname_small = os.path.dirname(os.path.abspath(__file__)) + "/19430526/STR_small_true"
+        pages_paths_small = glob.glob(pics_dirname_small + '/*.jpg')
+        pages_paths_small.sort()
+        
+        try:
+            num_pics = ImageExtractor.compress_images(pics_dirname)
+            self.assertEqual(len(pages_paths_small), num_pics)
+
+            for test_path in pages_paths_small:
+                new_path = page.replace('STR_small_true','STR_small')
+                # print(test_path, name)
+                test = Image.open(test_path)
+                created = Image.open(new_path)
+                self.assertEqual(created, test)
+
+                test_hist = test.histogram()
+                created_hist = created.histogram()
+                self.assertEqual(created_hist, test_hist)
+
+        finally:
+            shutil.rmtree(rem_dir)
